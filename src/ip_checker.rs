b685889cfd50ba989eee1b4acb7e7171effe1b9a
phanta <asdlaskrvbh.direction656@passfwd.com>
2024-01-01 02:43:55 +0100

    Improvements from Peelz

--- src/ip_checker.rs
+++ src/ip_checker.rs
@@ -1,36 +1,79 @@
-use anyhow::Error;
+use std::collections::HashMap;
+
+use eyre::{Context, Result};
 use maplit::hashmap;
-use reqwest::Client;
+use serde::Deserialize;
+use tokio::sync::RwLock;
 
-use crate::models::{IpQualityApiResponse, Player};
+#[derive(Deserialize, Debug, Clone)]
+pub struct IpQualityApiResponse {
+  pub country_code: String,
+  pub proxy: bool,
+  pub vpn: bool,
+}
 
-const IP_QUALITY_BASE_URL: &str = "https://www.ipqualityscore.com/api/json/ip";
-
-pub struct IpChecker;
+pub(crate) struct IpChecker {
+  client: reqwest::Client,
+  api_key: String,
+  cache: RwLock<HashMap<String, IpQualityApiResponse>>,
+}
 
 impl IpChecker {
-    pub async fn check_ip(
-        client: &Client,
-        player: &Player,
-        api_key: &str,
-    ) -> Result<IpQualityApiResponse, Error> {
-        let params = hashmap! {
-            "strictness" => "1",
-            "allow_public_access_points" => "true",
-            "lighter_penalties" => "true",
-            "mobile" => "true",
-        };
+  pub async fn get<S: AsRef<str>>(
+    &self,
+    ip: S,
+  ) -> Result<IpQualityApiResponse> {
+    const IP_QUALITY_BASE_URL: &str =
+      "https://www.ipqualityscore.com/api/json/ip";
 
-        let url = format!("{}/{}/{}", IP_QUALITY_BASE_URL, api_key, player.ip);
+    let ip = ip.as_ref();
 
-        let response = client
-            .get(&url)
-            .query(&params)
-            .send()
-            .await?
-            .json::<IpQualityApiResponse>()
-            .await?;
-
-        Ok(response)
+    // IMPORTANT: this block ensures that the cache lock is dropped before we do any async processing.
+    // If we were to hold the lock, no other thread could make use of the ip checker.
+    {
+      let cache = self.cache.read().await;
+      if let Some(data) = cache.get(ip) {
+        return Ok(data.clone());
+      }
     }
+
+    let params = hashmap! {
+        "strictness" => "1",
+        "allow_public_access_points" => "true",
+        "lighter_penalties" => "true",
+        "mobile" => "true",
+    };
+
+    let url = format!("{}/{}/{}", IP_QUALITY_BASE_URL, self.api_key, ip);
+
+    let response = self
+      .client
+      .get(&url)
+      .query(&params)
+      .send()
+      .await
+      .context("failed to check ip info")?
+      .json::<IpQualityApiResponse>()
+      .await
+      .context("failed to parse response")?;
+
+    let mut cache = self.cache.write().await;
+    if let Some(entry) = cache.get_mut(ip) {
+      *entry = response.clone()
+    }
+
+    Ok(response)
+  }
+
+  pub fn new<S>(client: reqwest::Client, api_key: S) -> Self
+  where
+    S: Into<String>,
+  {
+    let api_key = api_key.into();
+    Self {
+      api_key,
+      client,
+      cache: RwLock::new(HashMap::new()),
+    }
+  }
 }
